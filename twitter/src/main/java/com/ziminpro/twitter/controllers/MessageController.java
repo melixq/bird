2233183666b8646b44df28eb37f2b5b35f3acf61
melixq <61516790+melixq@users.noreply.github.com>
2026-01-05 20:21:29 +0500

    extract user id from security context in MessageController

--- twitter/src/main/java/com/ziminpro/twitter/controllers/MessageController.java
+++ twitter/src/main/java/com/ziminpro/twitter/controllers/MessageController.java
@@ -6,8 +6,8 @@ import java.util.UUID;
 import com.ziminpro.twitter.dtos.Constants;
 import com.ziminpro.twitter.dtos.Message;
 import com.ziminpro.twitter.services.MessagesService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,37 +18,48 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class MessageController {
+    private final MessagesService messages;
 
-    @Autowired
-    private MessagesService messages;
+    public MessageController(MessagesService messages) {
+        this.messages = messages;
+    }
 
     @RequestMapping(method = RequestMethod.GET, path = Constants.URI_MESSAGE + "/{message-id}")
-    public Mono<ResponseEntity<Map<String, Object>>> getMessagebyId(
-            @PathVariable(value = "message-id", required = true) String messageId) {
+    public Mono<ResponseEntity<Map<String, Object>>> getMessageById(
+            @PathVariable(value = "message-id") String messageId) {
         return messages.getMessagebyId(UUID.fromString(messageId));
     }
 
     @RequestMapping(method = RequestMethod.GET, path = Constants.URI_PRODUCER + "/{producer-id}")
     public Mono<ResponseEntity<Map<String, Object>>> getMessagesForProducerById(
-            @PathVariable(value = "producer-id", required = true) String producerId) {
+            @PathVariable(value = "producer-id") String producerId) {
         return messages.getMessagesForProducerById(UUID.fromString(producerId));
     }
 
     @RequestMapping(method = RequestMethod.GET, path = Constants.URI_SUBSCRIBER + "/{subscriber-id}")
     public Mono<ResponseEntity<Map<String, Object>>> getMessagesForSubscriberById(
-            @PathVariable(value = "subscriber-id", required = true) String subscriberId) {
+            @PathVariable(value = "subscriber-id") String subscriberId) {
         return messages.getMessagesForSubscriberById(UUID.fromString(subscriberId));
     }
 
+    @RequestMapping(method = RequestMethod.GET, path = "/messages/my-feed")
+    public Mono<ResponseEntity<Map<String, Object>>> getMyMessages(Authentication authentication) {
+        String userId = (String) authentication.getPrincipal();
+        return messages.getMessagesForSubscriberById(UUID.fromString(userId));
+    }
+
     @RequestMapping(method = RequestMethod.POST, path = Constants.URI_MESSAGE, consumes = Constants.APPLICATION_JSON)
-    public Mono<ResponseEntity<Map<String, Object>>> createMessage(@RequestBody Message message) {
+    public Mono<ResponseEntity<Map<String, Object>>> createMessage(
+            @RequestBody Message message,
+            Authentication authentication) {
+        String userId = (String) authentication.getPrincipal();
+        message.setAuthor(UUID.fromString(userId));
         return messages.createMessage(message);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, path = Constants.URI_MESSAGE + "/{message-id}")
     public Mono<ResponseEntity<Map<String, Object>>> deleteMessageById(
-            @PathVariable(value = "message-id", required = true) String messageId) {
+            @PathVariable(value = "message-id") String messageId) {
         return messages.deleteMessageById(UUID.fromString(messageId));
     }
-
 }
